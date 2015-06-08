e7feb8e918ccff3e17aa78f3349cc3cfefd96f7f
The Dan <danielgelbart@gmail.com>
2015-06-08 14:51:04 +0300

    removed contact cmailer

--- app/mailers/contact_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default from: "help@publicsecurities.com"
-
-  def welcome_email(to_mail)
-    @url  = 'http://example.com/login'
-    #this creates the actual mail to be sent
-    mail(to: to_mail, subject: 'Welcome to My Awesome Site')
-  end
-end
