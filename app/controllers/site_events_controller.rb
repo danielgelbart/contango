c3d9826f93765633ce72bb8ddf29fed7c0cf7492
The Dan <danielgelbart@gmail.com>
2015-10-19 16:43:24 +0300

    fixed site events

--- app/controllers/site_events_controller.rb
+++ app/controllers/site_events_controller.rb
@@ -69,6 +69,6 @@ class SiteEventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def site_event_params
-      params.require(:site_event).permit(:event, :event_date)
+      params.require(:site_event).permit(:event, :event_time)
     end
 end
